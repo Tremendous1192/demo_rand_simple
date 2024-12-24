f212e817b94bf8c260590ef6980cf739ed6e4ab2
Tremendous1192 <tremendous.duck@gmail.com>
2024-12-24 20:02:04 +0900

    Beta分布のデモ

--- examples/beta.rs
+++ examples/beta.rs
@@ -1,10 +1,14 @@
-mod common_functions;
+use csv::Writer;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // ---- 初期化する ----
     // 乱数生成器
     let mut generator =
         rand_simple::Beta::new([1192u32, 765u32, 1543u32, 2003u32, 1867u32, 1688u32]);
+    // CSVファイルのパス
+    let file_path = "C:/MyPrograms/demo_rand_simple/data/Beta.csv";
 
+    // ---- 乱数を生成する ----
     // 標準分布
     let mut standard_samples = Vec::<f64>::new();
     for _ in 0..10000 {
@@ -20,16 +24,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         custom_samples.push(generator.sample() as f64);
     }
 
-    // 乱数列ヒストグラムの描画
-    let _ = common_functions::draw_histgram(
-        "examples/beta.png",
-        "Beta distribution",
-        &standard_samples,
-        &custom_samples,
-        0f64..2f64,
-        0.1f64,
-        0..2_000,
-    );
+    // ---- 生成した乱数をCSVファイルに保存する ----
+    // CSVファイルWriterの初期化
+    let mut wtr = Writer::from_path(file_path)?;
+    // 列名の保存
+    wtr.write_record(["Standard distribution", "Custom parameter distribution"])?;
+    // 生成した乱数を保存する
+    for (sd, cpd) in standard_samples.iter().zip(custom_samples.iter()) {
+        wtr.write_record([sd, cpd].into_iter().map(|e| e.to_string()).clone())?;
+    }
+    // 書き込み完了時に行う
+    wtr.flush()?;
 
     Ok(())
 }
